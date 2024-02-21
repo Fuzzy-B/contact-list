2532c5b36d03488adad466be74cdf4e9d639c177
Ian Balderston <ianmb@vt.edu>
2024-02-21 14:06:51 -0500

    added table functionality with dummyContacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react' 
 import './App.css'
+import './components/ContactList.jsx'
+import ContactList from './components/ContactList.jsx'
+import { dummyContacts } from './components/ContactList.jsx'
 
 function App() {
-   
-
+  const [contacts, setContacts] = useState(dummyContacts)
+  console.log('contacts: ', contacts)
   return (
     <>
-       
+       <ContactList />
     </>
   )
 }
